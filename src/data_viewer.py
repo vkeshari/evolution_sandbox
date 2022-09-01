5cda63621d94b2261b1a4da5fa498b715992cd14
Vineet Keshari <vineet.keshari@gmail.com>
2022-09-01 10:55:08 -0700

    Remove filename

--- src/data_viewer.py
+++ src/data_viewer.py
@@ -1,6 +1,6 @@
 from metrics import dataio as dat
 
-FILENAME = "Run_p100_r10_i100_a10_CROSSOVER_BY_ASSIGNMENT_ONLY_False_False_20220901094226.data"
+FILENAME = ""
 
 def main():
   fhio = dat.FitnessHistoryIO()
